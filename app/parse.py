59848efa6a9d13187d6ee41dd5c5cc7254443fb4
Dmytro Minyaylo <dimon9660@gmail.com>
2025-09-19 19:38:46 +0300

    fix parse.py

--- app/parse.py
+++ app/parse.py
@@ -1,8 +1,13 @@
 import csv
-import dataclasses
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
+import time
 import requests
+from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+BASE_URL = "https://quotes.toscrape.com"
 
 
 @dataclass
@@ -12,16 +17,28 @@ class Quote:
     tags: list[str]
 
 
-BASE_URL = " https://quotes.toscrape.com/"
-QUOTE_FIELDS = [field.name for field in dataclasses.fields(Quote)]
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def get_page(page_number: int) -> bytes:
+    url = urljoin(BASE_URL, f"page/{page_number}/")
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        return response.content
+    except RequestException as e:
+        print(f"Request failed for {url}: {e}")
+        return b""
 
 
 def get_all_quotes() -> list[Quote]:
     collected_quotes = []
     page = 1
     while True:
-        response = requests.get(f"{BASE_URL}page/{page}/")
-        soup = BeautifulSoup(response.content, "html.parser")
+        html_content = get_page(page)
+        if not html_content:
+            break
+        soup = BeautifulSoup(html_content, "html.parser")
         quotes_on_page = soup.select("div.quote")
         if not quotes_on_page:
             break
@@ -32,18 +49,21 @@ def get_all_quotes() -> list[Quote]:
                 tags=[tag.text for tag in quote.select("div.tags a")]
             ))
         page += 1
+        time.sleep(0.2)
     return collected_quotes
 
 
-def write_quotes_to_csv(quotes: list[Quote], file_path: str) -> None:
-    with open(file_path, "w", encoding="utf-8", newline="") as file:
+def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str) -> None:
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(QUOTE_FIELDS)
-        writer.writerows([dataclasses.astuple(quote) for quote in quotes])
+        for quote in quotes:
+            writer.writerow([quote.text, quote.author, quote.tags])
 
 
-def main(file_path: str) -> None:
-    write_quotes_to_csv(get_all_quotes(), file_path)
+def main(output_csv_path: str) -> None:
+    quotes = get_all_quotes()
+    write_quotes_to_csv(quotes, output_csv_path)
 
 
 if __name__ == "__main__":
